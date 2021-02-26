3ae4955a2651383a98199b9c82b9976ee16f068d
Shota Imaki <shota.imaki.0801@gmail.com>
2021-02-26 12:07:00 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 python3 -m pytest --doctest-modules fracdiff
 python3 -m pytest --doctest-modules tests
 
 python3 -m flake8 fracdiff
-python3 -m black --check fracdiff || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black fracdiff
-python3 -m isort --check --force-single-line-imports fracdiff || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports fracdiff
+python3 -m black --check --quiet fracdiff || read -p "Run black? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m black --quiet fracdiff
+python3 -m isort --check --force-single-line-imports fracdiff || read -p "Run isort? (y/n): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports --quiet fracdiff
+python3 -m black --quiet tests
+python3 -m isort --force-single-line-imports --quiet tests
